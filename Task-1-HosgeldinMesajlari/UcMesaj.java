468d6d69799f959881f3f285f38109a374e725d4
Tahagkdr <250541092@firat.edu.tr>
2025-10-20 21:37:07 +0300

    Add UçMesaj class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UçMesaj {
+    public  static void main(String[] args) {
+        System.out.println("Javayahoşgeldiniz");
+        System.out.println("Bilgisayar Bilimine Ho¸s Geldiniz");
+        System.out.println("Programlama E˘glencelidir");
+    }
+}
+
