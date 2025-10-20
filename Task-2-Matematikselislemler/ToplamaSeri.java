7f548aa1582170d6e2c6adad9f3e0b29d35a7e99
Tahagkdr <250541092@firat.edu.tr>
2025-10-20 21:46:19 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,12 @@
+public class Toplamaseri {
+
+    public static void main(String[] args) {
+
+        // 1'den 9'a kadar olan sayıları doğrudan topluyoruz
+        // ve sonucu 'toplam' adında bir değişkende saklıyoruz.
+        int toplam = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        // Sonucu ekrana yazdırıyoruz.
+        System.out.println("İşlemin sonucu: " + toplam);
+    }
+}
