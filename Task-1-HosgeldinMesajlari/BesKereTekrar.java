72f51a2ad5cdb6684b0e771fd5f6ecfe45d8ed8f
Tahagkdr <250541092@firat.edu.tr>
2025-10-20 21:29:39 +0300

    Add BesKereTekrar.java to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class beşMesaj {
+    public  static void main(String args[]) {
+        System.out.println("javahoşgeldiniz");
+        System.out.println("javahoşgeldiniz");
+        System.out.println("javahoşgeldiniz");
+        System.out.println("javahoşgeldiniz");
+        System.out.println("javahoşgeldiniz");
+
+    }
+}
