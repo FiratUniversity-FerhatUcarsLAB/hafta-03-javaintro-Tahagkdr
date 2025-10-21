dc062eef073abbecc2fc86ccb06ba6df1a5a5772
Tahagkdr <250541092@firat.edu.tr>
2025-10-21 18:59:41 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class Dikdörtgenhesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+        System.out.println("Dikdörtgenin Eni:"+ en);
+        System.out.println("Dikdörgenin boyu:"+ boy);
+        System.out.println("hesaplanan alan:"+ alan);
+        System.out.println("hesaplanan cevre:"+ cevre);
+
+    }
