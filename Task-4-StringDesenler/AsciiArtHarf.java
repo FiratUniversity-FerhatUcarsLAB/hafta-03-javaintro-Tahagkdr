4ef010adcb101ccbf85c79213afd55ee48223410
Tahagkdr <250541092@firat.edu.tr>
2025-10-21 19:15:18 +0300

    Add AsciiArtHarf class for ASCII art representation

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class AsciiArtHarf {
+    public  static void main(String[] args) {
+        System.out.println("TTTTTTTTTTT");
+        System.out.println("TTTTTTTTTTT");
+        System.out.println("     TTT     ");
+        System.out.println("     TTT     ");
+        System.out.println("     TTT     ");
+        System.out.println("     TTT     ");
+        System.out.println("     TTT     ");
+    }
+}
